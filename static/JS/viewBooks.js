c8421897d5d14a6fbae99e8710d8951fb8245098
Gaurav Sorte <gaurav.sorte@joshsoftware.com>
2022-02-03 13:42:42 +0530

    added search queries in js

--- static/JS/viewBooks.js
+++ static/JS/viewBooks.js
@@ -6,35 +6,118 @@ serarch_filter.innerHTML += '<option class="dropdown_links" value="Title">Title<
 serarch_filter.innerHTML += '<option class="dropdown_links" value="Author">Author</option>'
 serarch_filter.innerHTML += '<option class="dropdown_links" value="ISBN">ISBN</option>'
 
-console.log('hii hello');
+// console.log('hii hello');
 
-// function filter_by_id(value){
-//     var table_rows = document.querySelectorAll('thead-dark')
-//     console.log(table_rows);
-// }
+function filter_by_id(value){
+    var table_rows = document.querySelectorAll('.table_value_')
+    if(value) {
+        console.log(table_rows);
+        for (let index = 0; index < table_rows.length; index++){
+            if(table_rows[index].children[0].innerText != value){
+                table_rows[index].classList.add('hidden')
+            } else {
+                table_rows[index].classList.remove('hidden')
+            }
+        }
+    } else {
+        for (let index = 0; index < table_rows.length; index++){
+                table_rows[index].classList.remove('hidden')
+        }
+    }
+    
+}
+
+function filter_by_title(value){
+    var table_rows = document.querySelectorAll('.table_value_')
+    if(value) {
+        console.log(table_rows);
+        for (let index = 0; index < table_rows.length; index++){
+            if(table_rows[index].children[1].innerText != value){
+                table_rows[index].classList.add('hidden')
+            } else {
+                table_rows[index].classList.remove('hidden')
+            }
+        }
+    } else {
+        for (let index = 0; index < table_rows.length; index++){
+                table_rows[index].classList.remove('hidden')
+        }
+    }
+    
+}
+
+
+function filter_by_author(value){
+    var table_rows = document.querySelectorAll('.table_value_')
+    if(value) {
+        console.log(table_rows);
+        for (let index = 0; index < table_rows.length; index++){
+            if(table_rows[index].children[2].innerText != value){
+                table_rows[index].classList.add('hidden')
+            } else {
+                table_rows[index].classList.remove('hidden')
+            }
+        }
+    } else {
+        for (let index = 0; index < table_rows.length; index++){
+                table_rows[index].classList.remove('hidden')
+        }
+    }
+    
+}
+
+
+function filter_by_ISBN(value){
+    var table_rows = document.querySelectorAll('.table_value_')
+    if(value) {
+        console.log(table_rows);
+        for (let index = 0; index < table_rows.length; index++){
+            if(table_rows[index].children[3].innerText != value){
+                table_rows[index].classList.add('hidden')
+            } else {
+                table_rows[index].classList.remove('hidden')
+            }
+        }
+    } else {
+        for (let index = 0; index < table_rows.length; index++){
+                table_rows[index].classList.remove('hidden')
+        }
+    }
+    
+}
 
 
 
 
-// function filter_by(selected_filter, value){
-//     if(selected_filter == "Member ID"){
-//         filter_by_id(value)
-//     } else if(selected_filter == "First Name"){
+function filter_by(selected_filter, value){
+    if(selected_filter == "Book ID"){
+        filter_by_id(value)
+    } else if(selected_filter == "Title"){
+        filter_by_title(value)
+    } else if(selected_filter == "Author") {
+        filter_by_author(value)
+    } else if(selected_filter == "ISBN") {
+        filter_by_ISBN(value)
+    }
 
-//     } else if(selected_filter == "Last Name") {
+}
 
-//     } else if(selected_filter == "Phone Number") {
-
-//     }
-
-// }
-
-// function filter() {
-//     let serarch_filter_val = document.getElementById('search_filter').value
-//     console.log('in func');
-//     let search_text = document.getElementById('search_input').value
-//     console.log('>>>> ',serarch_filter_val);
-// }
+function filter() {
+    let serarch_filter_val = document.getElementById('search_filter').value
+    if(!serarch_filter_val){
+        console.log('TRUE');
+        let table_rows = document.querySelectorAll('.table_value_')
+        for (let index = 0; index < table_rows.length; index++){
+            table_rows[index].classList.remove('hidden')
+        }
+    } else {
+        console.log(serarch_filter_val);
+        let search_text = document.getElementById('search_input').value
+        console.log('>>>> ',serarch_filter_val);
+        filter_by(serarch_filter_val, search_text)
+    }
+    
+}
 // console.log('hi there');
 
 // serarch_filter.onchange = filter();
--- static/JS/viewMembers.js
+++ static/JS/viewMembers.js
@@ -8,12 +8,91 @@ serarch_filter.innerHTML += '<option class="dropdown_links" value="Phone Number"
 
 
 function filter_by_member_id(value){
-    console.log('in filter');
+    // console.log('in filter');
     var table_rows = document.querySelectorAll('.table_value_')
-    for (let index = 0; index < table_rows.length; index++) {
-        console.log(table_rows[index].childNodes[0])
-        
+    if (value){
+        for (let index = 0; index < table_rows.length; index++) {
+            console.log(table_rows[index].children[0].innerText)
+            if(table_rows[index].children[0].innerText != value){
+                table_rows[index].classList.add('hidden')
+            } else {
+                table_rows[index].classList.remove('hidden')
+            }
+            
+        }
+    } else {
+        for (let index = 0; index < table_rows.length; index++) {
+            
+                table_rows[index].classList.remove('hidden')
+        }
     }
+    
+    console.log(table_rows);
+}
+
+function filter_by_first_name(value){
+    // console.log('in filter');
+    var table_rows = document.querySelectorAll('.table_value_')
+    if (value){
+        for (let index = 0; index < table_rows.length; index++) {
+            // console.log(table_rows[index].children[1].innerText)
+            if(table_rows[index].children[1].innerText != value){
+                table_rows[index].classList.add('hidden')
+            } else {
+                table_rows[index].classList.remove('hidden')
+            }
+            
+        }
+    } else {
+        for (let index = 0; index < table_rows.length; index++) {
+                table_rows[index].classList.remove('hidden')
+        }
+    }
+    
+    console.log(table_rows);
+}
+
+function filter_by_last_name(value){
+    // console.log('in filter');
+    var table_rows = document.querySelectorAll('.table_value_')
+    if (value){
+        for (let index = 0; index < table_rows.length; index++) {
+            if(table_rows[index].children[2].innerText != value){
+                table_rows[index].classList.add('hidden')
+            } else {
+                table_rows[index].classList.remove('hidden')
+            }
+            
+        }
+    } else {
+        for (let index = 0; index < table_rows.length; index++) {
+            
+                table_rows[index].classList.remove('hidden')
+        }
+    }
+    
+    console.log(table_rows);
+}
+
+function filter_by_phone_number(value){
+    // console.log('in filter');
+    var table_rows = document.querySelectorAll('.table_value_')
+    if (value){
+        for (let index = 0; index < table_rows.length; index++) {
+            if(table_rows[index].children[3].innerText != value){
+                table_rows[index].classList.add('hidden')
+            } else {
+                table_rows[index].classList.remove('hidden')
+            }
+            
+        }
+    } else {
+        for (let index = 0; index < table_rows.length; index++) {
+            
+                table_rows[index].classList.remove('hidden')
+        }
+    }
+    
     console.log(table_rows);
 }
 
@@ -24,10 +103,13 @@ function filter_by(selected_filter, value){
     if(selected_filter == "Member ID"){
         filter_by_member_id(value)
     } else if(selected_filter == "First Name"){
+        filter_by_first_name(value)
 
     } else if(selected_filter == "Last Name") {
+        filter_by_last_name(value)
 
     } else if(selected_filter == "Phone Number") {
+        filter_by_phone_number(value)
 
     }
 
